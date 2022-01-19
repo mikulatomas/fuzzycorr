d2d2997f70e770adc92c8dea3d003c3c35607e52
Tomáš Mikula <mail@tomasmikula.cz>
2022-01-19 12:01:51 +0100

    0.1.1 (#2)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fuzzycorr",
-    version="0.1.0",
+    version="0.1.1",
     author="Tomáš Mikula",
     author_email="mail@tomasmikula.cz",
     description="A numpy implementation of Robust Rank Correlation Coefficients.",
